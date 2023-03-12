6a5e142f84b19ac2367d36813678f454b31a0fea
Keara Harris <keara.s.harris@gmail.com>
2023-03-12 00:05:46 -0600

    Completed into to functions lab

---
+++ index.js
@@ -0,0 +1,31 @@
+//Two functions below RETURN the value of the upper or lowercase string
+function shout(string){
+    return string.toUpperCase();
+}
+
+function whisper(string){
+    return string.toLowerCase();
+
+}
+//Functions below PRINT OUT string in lower or uppercase
+function logShout(string){
+    console.log(string.toUpperCase());
+
+}
+function logWhisper(string){
+    console.log(string.toLowerCase());
+
+}
+
+function sayHiToHeadphonedRoommate(string){
+    if(string === string.toLowerCase()){
+        return "I can't hear you!";
+    }
+    else if(string === string.toUpperCase()){
+        return "YES INDEED!";
+    }
+    else if(string === "Let's have dinner together!"){
+        return "I would love to!";
+
+    }
+}
